314c367d4b8b5ad7093ce92e6605d5ab2bdcceda
Amilton Diego <amiltondiego@gmail.com>
2022-03-19 15:14:33 -0300

    create model table

---
+++ app/Models/IndentifyRegister.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IndentifyRegister extends Model
+{
+    use HasFactory;
+
+    public bool $timestamps = false;
+}
---
+++ database/migrations/2022_03_19_172740_create_indentify_register_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('indentify_register', function (Blueprint $table) {
+            $table->id();
+            $table->integer('indentify');
+            $table->dateTimeTz('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('indentify_register');
+    }
+};
